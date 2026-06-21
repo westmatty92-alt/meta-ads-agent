e17d2cd9ff16c90a01e1ed6cded83133a8b2c9bf
Matt West <west.matty92@gmail.com>
2026-06-21 15:47:48 -0400

    chore: add version comment to index.html, add ETag and max-age=0 to server.py NoCacheHandler

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import http.server
 import socketserver
+import os
 
 class NoCacheHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
+        self.send_header("ETag", str(os.path.getmtime(self.translate_path(self.path))))
         super().end_headers()
 
     def log_message(self, format, *args):
